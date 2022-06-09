f805057fa65e586c51e493080410073ea718d735
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-06-09 21:37:44 +0000

    unfinished

--- tauCircumference.cpp
+++ tauCircumference.cpp
@@ -5,23 +5,39 @@
 // Calculates perimeter and area of a rectangle
 //     with dimensions inputted from user
 
+#include <cmath>
 #include <iostream>
+
+float roundNumber(int decimalsNumber, float &number) {
+    number = number * pow(10, decimalsNumber) + 0.5;
+    int tempNumber = (int)number;
+    number = (float)tempNumber / pow(10, decimalsNumber);
+}
 
 int main() {
     // I calculate circumference of a circle
-    const double TAU = 6.28;
-    int radius;
-    double circumference;
+    std::string strDecimalsNumber;
+    std::string strNumber;
+    int intDecimalsNumber;
+    float fltNumber;
 
     // input
-    std::cout << "Enter radius of the circle in mm: ";
-    std::cin >> radius;
+    std::cout << "gib decimals number: ";
+    std::cin >> strDecimalsNumber;
+    std::cout << "gib number: ";
+    std::cin >> strNumber;
 
     // process
-    circumference = TAU*radius;
+    try {
+        intDecimalsNumber = std::stoi(strDecimalsNumber);
+        fltNumber = std::stof(strNumber);
+        roundNumber(intDecimalsNumber, fltNumber);
+    } catch (std::invalid_argument) {
+        std::cout << "Invalid Input" << std::endl;
+    }
 
     // output
     std::cout << "" << std::endl;
-    std::cout << "Circumference is " << circumference << " mm" << std::endl;
+    std::cout << "Circumference is " << fltNumber << " mm" << std::endl;
     std::cout << "\nDone." << std::endl;
 }
